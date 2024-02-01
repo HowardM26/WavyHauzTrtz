412c0f51330115ceced7488c949288c99e15057f
Howard Mell <126512433+HowardM26@users.noreply.github.com>
2024-01-31 22:51:59 -0500

    added square link to cards

--- WavyHauz/my-react-app/src/components/Cards.js
+++ WavyHauz/my-react-app/src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
+import { Link } from 'react-router-dom';
 
 function Cards() {
   return (
@@ -13,27 +14,28 @@ function Cards() {
              src='/WavyHauzTrts_pics/Ohhhhhhreeooos.jpeg'
              text='Mouthwatering cookies and creme flavor! $2.50'
              label='Sweet'
-             path='/products'
+             path='https://square.link/u/BcG1w0pd'
+             
             />
             <CardItem
               src='/WavyHauzTrts_pics/Macadamia Madness.jpeg'
-              text='Chewy buttery cookie with white chocolate chunks, crunchy macadamia nuts and raspberry jam?? $2.50'
+              text='Chewy buttery cookie with white chocolate chunks, crunchy macadamia nuts and raspberry jam?? $2.75'
               label='sweet'
-              path='/products'
+              path='https://square.link/u/wP4yqldW'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
               src='/WavyHauzTrts_pics/Salty Scotch.jpeg'
-              text='You have to taste it to believe it $5'
+              text='You have to taste it to believe it $2'
               label='Sweet/Savory'
-              path='/products'
+              path='https://square.link/u/XOAF5ToH'
             />
             <CardItem
               src='/WavyHauzTrts_pics/Goldies.jpeg'
-              text='Goldies $1.75'
+              text='Goldie $2.50'
               label='Hearty'
-              path='/products'
+              path='https://square.link/u/s1XgAX6c'
             />
         
           </ul>
