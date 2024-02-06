2ee2ba3026e3ba682784a904fd25155d879cc5c3
Howard Mell <126512433+HowardM26@users.noreply.github.com>
2024-02-06 17:57:19 -0500

    Updates

---
+++ precache-manifest.4445bad986b5c2922d8ee2a639b10e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b497e414ef2b8d5661906a0d14ce3040",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "e9a831f9d4d76d116056",
+    "url": "/my-react-app/static/css/2.6f2db2d8.chunk.css"
+  },
+  {
+    "revision": "5941f0b56cb6c5921a36",
+    "url": "/my-react-app/static/css/main.eddef21e.chunk.css"
+  },
+  {
+    "revision": "e9a831f9d4d76d116056",
+    "url": "/my-react-app/static/js/2.41d77088.chunk.js"
+  },
+  {
+    "revision": "5941f0b56cb6c5921a36",
+    "url": "/my-react-app/static/js/main.89bee83b.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  }
+]);
