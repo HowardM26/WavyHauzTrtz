43346f0916569a1f870615f7b6363e349f1701b2
Howard Mell <mell_howard@yahoo.com>
2025-01-13 14:01:51 -0500

    Redid the index.html and App.js

--- WavyHauz/my-react-app/src/App.js
+++ WavyHauz/my-react-app/src/App.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Navbar from './components/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 import GourmetCookies from './components/pages/GourmetCookies';
 import CustomOrder from './components/pages/CustomOrder';
-import withSplashScreen from './components/withSplashScreen'; 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
+import withSplashScreen from './components/withSplashScreen';
 
 function App() {
   return (
-    <>
     <Router>
-        <Navbar />
-        <Routes>
-        
-          <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About />} />
-       
-          <Route path='/GourmetCookies' element={<GourmetCookies />} />
-          <Route path='/CustomOrder' element={<CustomOrder />} />
-        </Routes>
-      </Router>
-      
-  </>
-);
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/GourmetCookies" element={<GourmetCookies />} />
+        <Route path="/CustomOrder" element={<CustomOrder />} />
+      </Routes>
+    </Router>
+  );
 }
-export default withSplashScreen(App);   
+
+export default withSplashScreen(App);
