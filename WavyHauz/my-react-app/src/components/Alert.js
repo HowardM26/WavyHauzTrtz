dc607a0788e14d05ee519d33e2561fd46e036499
Howard Mell <mell_howard@yahoo.com>
2025-01-14 13:57:17 -0500

    added propTypes to the Alert.js

--- WavyHauz/my-react-app/src/components/Alert.js
+++ WavyHauz/my-react-app/src/components/Alert.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';  
-import {Container, Button , Alert} from 'react-bootstrap';  
-function AlertMessage() {  
-   const [show, setShow] = useState(true)  
-if (show){  
-  return (  
-    <div className="App">  
-   <Container className='p-4'>  
-   <Alert variant="success" onClose={() => setShow(false)} dismissible >  
-   <Alert.Heading>All custom orders should be placed A WEEK in advance, as they come second priority to regular orders,
-          especially if they are not on the regular menu for the month</Alert.Heading>  
-  
-   </Alert>  
-</Container>  
-    </div>  
-  );  
-}  
-return(  
-  <>  
-  <Button className='p-3 m-3' variant='primary' onClick={() => setShow(true)}>Order Instructions</Button>  
-  </>  
-)  
-}  
-export default AlertMessage; 
+import { Container, Button, Alert } from 'react-bootstrap';
+
+function AlertMessage({ message, variant = "success" }) {  
+  const [show, setShow] = useState(true);
+
+  if (show) {  
+    return (  
+      <div className="App">  
+        <Container className="p-4">
+          <Alert variant={variant} onClose={() => setShow(false)} dismissible>
+            <Alert.Heading>{message}</Alert.Heading>  
+          </Alert>
+        </Container>
+      </div>  
+    );
+  }
+  return (
+    <>
+      <Button className="p-3 m-3" variant="primary" onClick={() => setShow(true)}>
+        Order Instructions
+      </Button>
+    </>
+  );
+}
+// Add PropTypes validation for props
+AlertMessage.propTypes = {
+  message: PropTypes.string.isRequired, // Validate that 'message' is a required string
+  variant: PropTypes.string.isRequired, // Validate that 'variant' is a string
+}
+
+export default AlertMessage;
