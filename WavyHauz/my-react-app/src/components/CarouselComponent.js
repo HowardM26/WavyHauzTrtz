e8568032246eae06750ffeed63d935e2edc89deb
Howard Mell <mell_howard@yahoo.com>
2025-01-15 09:38:40 -0500

    removed products and sign up pages

--- WavyHauz/my-react-app/src/components/CarouselComponent.js
+++ WavyHauz/my-react-app/src/components/CarouselComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CarouselComponent = () => {
   return (
@@ -7,33 +8,33 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=Slide+1"
-          alt="First slide"
+          src="/WavyHauzTrts_pics/Ohhhhhhreeooos.jpg"
+          alt="Ohhhhhhreeooos"
         />
         <Carousel.Caption>
-          <h3>First Slide</h3>
+          <h3>Ohhhhhhreeooos</h3>
           <p>This is a description for the first slide.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=Slide+2"
-          alt="Second slide"
+          src="/WavyHauzTrts_pics/Apple_Spice.jpg"
+          alt="Apple Spice"
         />
         <Carousel.Caption>
-          <h3>Second Slide</h3>
+          <h3>Apple Spice</h3>
           <p>This is a description for the second slide.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://via.placeholder.com/800x400?text=Slide+3"
-          alt="Third slide"
+          src="/WavyHauzTrts_pics/Macadamia_Madness.jpg"
+          alt="Macadamia Madness"
         />
         <Carousel.Caption>
-          <h3>Third Slide</h3>
+          <h3>Macadamia Madness</h3>
           <p>This is a description for the third slide.</p>
         </Carousel.Caption>
       </Carousel.Item>
--- WavyHauz/my-react-app/src/components/pages/Home.js
+++ WavyHauz/my-react-app/src/components/pages/Home.js
@@ -2,47 +2,13 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-import { Carousel } from 'react-bootstrap';
+import CarouselComponent from '../CarouselComponent';
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First Slide Title</h3>
-            <p>Some description for the first slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second Slide Title</h3>
-            <p>Some description for the second slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third Slide Title</h3>
-            <p>Some description for the third slide.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <CarouselComponent />
       <Footer />
     </>
   );
